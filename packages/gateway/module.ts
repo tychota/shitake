aa157d704b460824e5549689c98bae3ca52d87fa
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2019-04-04 10:37:34 +0200

    :sparkles: (gateway) install pino logger

--- packages/gateway/module.ts
+++ packages/gateway/module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { UserModule } from '@shitake/microservice-user/infrastructure/api/module';
 
 import { ApplicationController } from './controller';
+
+import pino from 'pino-http';
 
 @Module({
   imports: [UserModule],
   controllers: [ApplicationController],
 })
-export class ApplicationModule {}
+export class ApplicationModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        pino({
+          // Temporary hack
+          // TODO: configure nest to use pino logger and then pipe to pretty-print
+          prettyPrint: {
+            levelFirst: true,
+          },
+        }),
+      )
+      .forRoutes('*');
+  }
+}
