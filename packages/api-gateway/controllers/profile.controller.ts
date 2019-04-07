f925ba46a2248922d3f18c4f03f133eca81f0dfe
tychota <tychota@users.noreply.github.com>
2019-04-07 16:40:20 +0200

    :recycle: (gateway) change user to profile

---
+++ packages/api-gateway/controllers/profile.controller.ts
@@ -0,0 +1,35 @@
+import { Observable } from 'rxjs';
+
+import { Controller, Get, OnModuleInit, Post, Param, Body, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ClientGrpc, Client } from '@nestjs/microservices';
+
+import { profileClientOptions } from '@shitake/microservice-profile/infrastructure/';
+import { ProfileDto } from '@shitake/microservice-profile/domain/dto';
+
+interface ProfileQuery {
+  hello(data: { object: string }): Observable<{ greetings: string }>;
+}
+
+interface ProfileCommand {
+  createProfile(dto: ProfileDto): Observable<void>;
+}
+
+@Controller('profile')
+export class ProfileGatewayController implements OnModuleInit {
+  @Client(profileClientOptions)
+  private readonly profileClient!: ClientGrpc;
+
+  private profileQuery!: ProfileQuery;
+  private profileCommand!: ProfileCommand;
+
+  onModuleInit() {
+    this.profileQuery = this.profileClient.getService<ProfileQuery>('Query');
+    this.profileCommand = this.profileClient.getService<ProfileCommand>('Command');
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async createProfile(@Body() dto: ProfileDto) {
+    return this.profileCommand.createProfile(dto);
+  }
+}
--- packages/api-gateway/module.ts
+++ packages/api-gateway/module.ts
@@ -1,15 +1,15 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 
-import { UserModule } from '@shitake/microservice-user/user.module';
-
-import { UserGatewayController } from './controllers/user.controller';
-
 import { EventstoreModule } from '@shitake/storage-eventstore/eventstore.module';
+import { ProfileModule } from '@shitake/microservice-profile';
+
+import { ProfileGatewayController } from './controllers/profile.controller';
+
 import { RequestIdMiddleware } from './middlewares/reqId.middleware';
 
 @Module({
-  imports: [UserModule, EventstoreModule],
-  controllers: [UserGatewayController],
+  imports: [ProfileModule, EventstoreModule],
+  controllers: [ProfileGatewayController],
 })
 export class ApplicationModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
