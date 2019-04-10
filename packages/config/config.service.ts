8ce4d93d08c8fd13677289165d072a3ad00049b4
tychota <tychota@users.noreply.github.com>
2019-04-11 01:11:51 +0200

    :sparkles: (config) add config service

---
+++ packages/config/config.service.ts
@@ -0,0 +1,36 @@
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
+import * as fs from 'fs';
+
+export interface EnvConfig {
+  [key: string]: string;
+}
+
+export class ConfigService {
+  private readonly envConfig: EnvConfig;
+
+  public constructor(filePath: string) {
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      NODE_ENV: Joi.string()
+        .valid(['development', 'production', 'test', 'provision'])
+        .default('development'),
+      PORT: Joi.number().default(3000),
+      API_AUTH_ENABLED: Joi.boolean().required(),
+    });
+
+    const { error, value: validatedEnvConfig } = Joi.validate(envConfig, envVarsSchema);
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
+  }
+}
